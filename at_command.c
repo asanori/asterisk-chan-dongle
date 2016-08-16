401b0c800273b9f6833bf4e0721d2d5b966d4b69
asanori <asanori@users.noreply.github.com>
2016-08-16 13:08:06 +0900

    Update at_command.c

--- at_command.c
+++ at_command.c
@@ -166,9 +166,9 @@ EXPORT_DEF int at_enque_initialization(struct cpvt* cpvt, at_cmd_t from_command)
 		ATQ_CMD_DECLARE_STI(CMD_AT_CSCS, cmd21),	/* UCS-2 text encoding */
 
 		ATQ_CMD_DECLARE_ST(CMD_AT_CPMS, cmd22),		/* SMS Storage Selection */
-		ATQ_CMD_DECLARE_DYN(CMD_AT_PORTSEL),	/* pvt->initialized = 1 after successful of CMD_AT_CNMI */
 		ATQ_CMD_DECLARE_ST(CMD_AT_CNMI, cmd23),		/* New SMS Notification Setting +CNMI=[<mode>[,<mt>[,<bm>[,<ds>[,<bfr>]]]]] */
 		ATQ_CMD_DECLARE_ST(CMD_AT_CSQ, cmd24),		/* Query Signal quality */
+		ATQ_CMD_DECLARE_DYN(CMD_AT_PORTSEL),	/* pvt->initialized = 1 after successful of CMD_AT_CNMI */
 		};
 	unsigned in, out;
 	int begin = -1;
